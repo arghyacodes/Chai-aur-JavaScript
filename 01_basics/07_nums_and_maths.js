a43b8cfa0384b315c1a4af09d85b1c7f73d1eaf5
Arghya Ghoshal <arghyaghoshal.juetce@gmail.com>
2024-07-26 13:38:20 +0530

    maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -1,1 +1,48 @@
-console.log("Hello");
+/*
+const roll = 24
+const balance = new Number(17500) //objectified as number
+
+console.log(roll);
+console.log(balance);
+console.log(balance.toFixed(2));
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+const score = 98.6885
+console.log(score.toPrecision(1));//prioritise number before decimal point
+console.log(score.toPrecision(2));//prioritise number before decimal point
+console.log(score.toPrecision(3));//prioritise number before decimal point
+console.log(score.toPrecision(4));//prioritise number before decimal point
+console.log(score.toPrecision(5));//prioritise number before decimal point
+console.log(score.toPrecision(6));//prioritise number before decimal point
+*/
+/*
+const hundreads = 8000000000000
+console.log(hundreads.toLocaleString('en-US'))//default is us standards
+console.log(hundreads.toLocaleString('en-IN'))//indian standards
+*/
+
+/*
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+*/
+
+//**********************************Maths*****************************************
+/*
+// comes by default with Js
+console.log(Math);
+
+console.log(Math.abs(-6));//absolute
+console.log(Math.round(4.5));//round off
+console.log(Math.ceil(4.2));//upper value
+console.log(Math.floor(4.9));//lower value
+console.log(Math.sqrt(25));
+console.log(Math.pow(5,2));
+console.log(Math.max(2,4,6,5,3,1));
+console.log(Math.min(2,4,6,1,3,5));
+*/
+
+console.log(Math.random());//always comes b/w 0 and 1
+console.log(Math.random()*10 + 1);//to get values b/w 1 to 10
