e652172be25aca9fddab7f29f6aa0f3c95f4723b
Arghya Ghoshal <arghyaghoshal.juetce@gmail.com>
2024-07-29 20:27:13 +0530

    js file execution

---
+++ 03_basics/05_execution.js
@@ -0,0 +1,16 @@
+/*
+JavaScript code -> Global Execution Context
+*Global EC
+*Function EC
+*Eval EC
+JavaScript code runs in two phases -
+1> Memory Creation Phase
+2> Execution Phase
+*/
+//*************WATCH VIDEO************************ */
+/*
+global ec-->memory phase(variables are created in memory with value "undefined")(definition of functions are loaded in memory)
+memory phase --> execution phase (variables are assigned values)
+if there is any function to be executed in execution phase then -->
+(function stack is called & one thread is assigned for function execution) function ececution context --> memory phase -->execution phase
+*/
