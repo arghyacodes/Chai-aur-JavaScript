02465835a6127782616cfe08ee5cd96613573702
Arghya Ghoshal <arghyaghoshal.juetce@gmail.com>
2024-07-29 21:02:53 +0530

    switch case

---
+++ 04_control_flow/switchcase.js
@@ -0,0 +1,21 @@
+
+/*
+switch (key) {
+    case key1: 
+        
+        break;
+    case key2: 
+        
+        break;
+    case key3: 
+        
+        break;
+    case key4: 
+        
+        break;
+
+    default:
+        break;
+}
+*/
+//without break statement it executes all the cases except default
