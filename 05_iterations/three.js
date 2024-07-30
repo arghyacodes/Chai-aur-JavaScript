654fed4213e2252628f03f4310d49d33fb297975
Arghya Ghoshal <arghyaghoshal.juetce@gmail.com>
2024-07-30 12:46:30 +0530

    special loops for arrays

---
+++ 05_iterations/three.js
@@ -0,0 +1,11 @@
+//forof loop
+const myArr = [10,20,30,40,50,60,70,80,90]
+
+for (const i of myArr) {
+    // console.log(i);
+}
+
+const greeting = "Hello World!"
+for (const greet of greeting) {
+    console.log(greet);
+}
