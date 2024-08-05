40af890872a02ff49371a92427db9064efae9392
Arghya Ghoshal <arghyaghoshal.juetce@gmail.com>
2024-08-05 22:17:30 +0530

    chin tapak dom dom

--- 06_dom/index.js
+++ 06_dom/index.js
@@ -1,3 +1,6 @@
-const myLst = document.querySelectorAll('li')
+// const myLst = document.querySelectorAll('li')
 
-myLst.forEach((l)=>l.style.backgroundColor = 'green')
+// // myLst.forEach((l)=>l.style.backgroundColor = 'green')
+// myLst[0].style.color = 'red';
+// myLst[1].style.color = 'green';
+// myLst[2].style.color = 'blue';
